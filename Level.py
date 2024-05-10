7f1098d8a90148b3807358b7eeac10f419909c33
Marco Maciel <marcomaciel06@gmail.com>
2024-05-10 16:59:48 -0300

    fiz a parte weapons

--- Level.py
+++ Level.py
@@ -6,6 +6,7 @@ from player import Player
 from debug import debug
 from suporte import *
 from random import choice
+from Weapon import Weapon
  
 
 
@@ -21,6 +22,9 @@ class Level:
 		# sprite group setup
 		self.visible_sprites = YSortCameraGroup()
 		self.obstacle_sprites = pygame.sprite.Group()
+
+		# attack sprites
+		self.current_attack = None
 
 		# sprite setup
 		self.create_map()
@@ -51,7 +55,16 @@ class Level:
 						if style == 'object':
 							surf = graphics['object'][int(col)]
 							Tile((x,y),[self.visible_sprites,self.obstacle_sprites], 'object', surf)
-		self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites)
+		self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites,self.create_attack,self.destroy_attack)
+
+	def create_attack(self):
+		self.current_attack = Weapon(self.player,[self.visible_sprites])
+
+	def destroy_attack(self):
+		if self.current_attack:
+			self.current_attack.kill()
+		self.current_attack = None
+
 
 	def run(self):
 		# update and draw the game
