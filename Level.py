45580b22c6f4e76e015885553fa181622e773fc4
GalileuCMMoares <165906088+GalileuCMMoares@users.noreply.github.com>
2024-05-09 22:12:12 -0300

    Quinto commit
    Add Level, tile e e mudança na main

---
+++ Level.py
@@ -0,0 +1,24 @@
+import pygame
+from configuraçao import*
+from tile import Tile
+from player import Player
+
+class Level:
+    def __init__(self):
+        self.display_surface=pygame.display.get_surface()
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORD_MAP):
+            for col_index, col in enumerate(row):
+                x=col_index*TILESIZE
+                y=row_index*TILESIZE
+                if col=='x':
+                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
+                if col=='p':
+                    Player((x,y),[self.visible_sprites])
+
+    def run(self):
+        self.visible_sprites.draw(self.display_surface)
+        
